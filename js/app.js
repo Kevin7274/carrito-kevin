40c8a03ad65758179482b610e5304a1f8e89a9ae
Kevin <kuhes0918@gmail.com>
2022-10-07 12:14:40 -0500

    otro commit

--- js/app.js
+++ js/app.js
@@ -4,13 +4,13 @@ const listaCursos = document.querySelector('#lista-cursos');
 const contenedorCarrito = document.querySelector('#vaciar-carrito');
 const variarCarrito = document.querySelector('#vaciar-carrito');
 
-let listadoCarrito = [1];
+let listadoCarrito = [];
 
 const agregarCurso = (e) => {
     e.preventDefault();
     //e.target.class.list.contains('agregar-carrito')
     if(e.target.classList.contains('agregar-carrito')){
-        const curso = e.target.parentElement
+        const curso = e.target.parentElement.parentElement;
         const infocurso = {
             imagen: curso.querySelector('img').src,
             nombre: curso.querySelector('h4').textContent,
@@ -24,10 +24,43 @@ const agregarCurso = (e) => {
 }
 
 const agregarCarrito = curso => {
-    listadoCarrito = [...listadoCarrito, curso]
+    //console.log("Curso a agregar");
+    //console.log(curso.id)
+    //console.log("listado de cursos")
+    //listadoCarrito.forEach(CURSO => console.log(CURSO.id));
+    let carrito = listadoCarrito.map(cursoInCarrito=>{
+        if(cursoInCarrito.id === curso.id){
+            cursoInCarrito.cantidad++;
+            return cursoInCarrito;
+        } else {
+            return cursoInCarrito;
+        }})
+    
+        listadoCarrito = [...carrito, curso]
     console.log(listadoCarrito);
+    generaHTML();
 }
+const generaHTML = () => {
+    vaciarCarrito();
+    listadoCarrito.forEach(curso => {
+        const row = document.createElement('tr');
+        const cursoHTML = `
+        <td>
+           <img src="${curso.imagen}"width=100>
+        </td> 
+        <td>${curso.nombre}</td>
+        <td>${curso.precio}</td>
+        <td>${curso.cantidad}</td>
 
+        `;
+        row.innerHTML = cursoHTML;
+        contenedorCarrito.appendChild(row);
+    });
+
+}
+const vaciarCarrito = () => {
+
+}
 const cargarEventListener = () => {
     //agregar funcion de carga de cursod al carrito
     listaCursos.addEventListener('click', agregarCurso);
